85d7399c3fe290583f1faec36a459edc4a57f682
ErnoMucha <140376845+ErnoMucha@users.noreply.github.com>
2023-08-02 14:27:47 +0200

    Item pick up method started.

--- OOP theory/Assets/Scripts/PlayerController.cs
+++ OOP theory/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 10;
+    private bool isItemTouched;
+
+    public GameObject holdItem;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isItemTouched)
+        {
+            //Pick up item.
+            Debug.Log("pick up");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Item"))
+        {
+            isItemTouched = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Item"))
+        {
+            isItemTouched = false;
+        }
+    }
+
+    void MovePlayer()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
     }
 }
