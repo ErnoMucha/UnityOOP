0323a20846af80a3c399e87f397cdf1409367958
ErnoMucha <140376845+ErnoMucha@users.noreply.github.com>
2023-08-03 09:38:34 +0200

    Add paint function.

--- OOP theory/Assets/Scripts/PaintMachine.cs
+++ OOP theory/Assets/Scripts/PaintMachine.cs
@@ -11,7 +11,13 @@ public class PaintMachine : Machine
         //Paint an existing item.
         if (IsTurnedOn)
         {
+            Renderer renderer = item.GetComponent<Renderer>();
+            float r = Random.Range(0, 1.0f);
+            float g = Random.Range(0, 1.0f);
+            float b = Random.Range(0, 1.0f);
 
+            renderer.material.color = new Color(r, g, b, 0);
+            item.transform.position = newItemPosition.transform.position;
         }
     }
 }
