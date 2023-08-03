92ed3040aa709d0b9da910185808f9f7f49bce5a
ErnoMucha <140376845+ErnoMucha@users.noreply.github.com>
2023-08-03 09:18:47 +0200

    Add paint machine to the scene.
    Painting not implemented yet.

---
+++ OOP theory/Assets/Scripts/PaintMachine.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// INHERITANCE
+public class PaintMachine : Machine
+{
+    public override void Use(GameObject item)
+    {
+        //Call: PlayerController.cs
+        //Paint an existing item.
+        if (IsTurnedOn)
+        {
+
+        }
+    }
+}
